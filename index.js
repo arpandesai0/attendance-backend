999e8408d2fbc77076798343514551c4bd3376fc
Arpan Desai <arpandesai0@gmail.com>
2022-03-10 20:39:38 +0530

    pdf compiler integrated with api

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Server running on " + PORT);
+});
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//defining routes
+const createRouter = require("./routes/create.router");
+app.use("/create", createRouter);
--- routes/create.router.js
+++ routes/create.router.js
@@ -3,30 +3,29 @@ const router = require("express").Router();
 const hbs = require("handlebars");
 const path = require("path");
 const fs = require("fs-extra");
-const test = {
-  department: "Electrical & Electronics Engineering",
-  name: "EEL201",
-  semester: "6",
-  statslist: [
-    { rollno: "BT19EEE068", totAbsentee: 0, totPresentee: 3 },
-    { rollno: "BT19EEE069", totAbsentee: 2, totPresentee: 1 },
-    { rollno: "BT19EEE070", totAbsentee: 0, totPresentee: 3 },
-  ],
-};
+
+//hbs compiler for dynamic pdf
 const complile = async (tmp, data) => {
   const tmppath = path.join(process.cwd(), "templates", `${tmp}.hbs`);
   const html = await fs.readFile(tmppath, "utf-8");
   return hbs.compile(html)(data);
 };
-const getPdf = async () => {
+hbs.registerHelper("add", (a, b) => {
+  return parseInt(a) + parseInt(b);
+});
+hbs.registerHelper("percentage", (a, b) => {
+  return parseInt((parseInt(a) / (parseInt(a) + parseInt(b))) * 100);
+});
+//pdf generator
+const getPdf = async (data) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const content = await complile("attendance", test);
+    const content = await complile("attendance", data);
     await page.setContent(content);
     // await page.emulate()
     await page.pdf({
-      path: "test.pdf",
+      path: __dirname + "/test.pdf",
       format: "A4",
       printBackground: true,
       margin: {
@@ -38,10 +37,32 @@ const getPdf = async () => {
     });
     console.log("Generated pdf successfully");
     await browser.close();
-    process.exit();
   } catch (error) {
     console.log(error);
   }
 };
-getPdf();
-router.get();
+const test = {
+  department: "Electrical & Electronics Engineering",
+  name: "EEL201",
+  semester: "6",
+  statslist: [
+    { rollno: "BT19EEE068", totAbsentee: 0, totPresentee: 3 },
+    { rollno: "BT19EEE069", totAbsentee: 2, totPresentee: 1 },
+    { rollno: "BT19EEE070", totAbsentee: 0, totPresentee: 3 },
+  ],
+};
+//api route
+router.get("/", (req, res) => res.status(200).send("Method not allowed"));
+router.post("/", (req, res) => {
+  const data = req.body;
+  try {
+    getPdf(data).then(() => {
+      const path = `${__dirname}\\test.pdf`;
+      return res.status(200).download(path);
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(404);
+  }
+});
+module.exports = router;
